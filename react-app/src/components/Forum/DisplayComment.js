65490e47991ce45a6a2101b3a771f0f00c11d375
Dusty Mumphrey <85353431+Dusttoo@users.noreply.github.com>
2021-11-17 16:58:13 -0600

    finished add comment form

--- react-app/src/components/Forum/DisplayComment.js
+++ react-app/src/components/Forum/DisplayComment.js
@@ -13,10 +13,11 @@ const DisplayComments = () => {
     const authors = useSelector(state => state.users)
     const comments = useSelector(state => state.comments)
     const currentUser = useSelector(state => state.session.user)
-
+    let author = {}
     const thisComment = Object.values(comments).find(comment => +comment.post_id === +postId)
-
-    const author = Object.keys(authors).find(thisAuthor => +thisComment.user_id === +thisAuthor)
+    if(thisComment) {
+        author = Object.keys(authors).find(thisAuthor => +thisComment.user_id === +thisAuthor)
+    }
     
     const deleteComment = () => {
         dispatch(removeComment(postId));
@@ -28,7 +29,7 @@ const DisplayComments = () => {
 
     return (
         <>
-            
+            {!thisComment ? <h2>No comments to display</h2>:
                <tr>  
                     <Link to={`/users/${author}`}><td><img className="profile-icon" src={authors[author].profile_img } alt={authors[author].name}/></td></Link>
                     <td>{thisComment.posted}</td>
@@ -40,7 +41,7 @@ const DisplayComments = () => {
                         className='delete-post'
                         onClick={deleteComment}>Delete</button>
                     </td> : <></>}
-                 </tr>  
+                 </tr>  }
         </>
     )
 }
