a4894f6bf756f632b25e2de4ffde56c1cbc18d19
Dusty Mumphrey <85353431+Dusttoo@users.noreply.github.com>
2021-11-20 13:51:18 -0600

    added logic for login logout button

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,10 +1,12 @@
 
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 import './NavBar.css'
 
 const NavBar = () => {
+  const currentUser = useSelector(state => state.session.user)
   return (
     <nav >
       <ul className="nav-bar">
@@ -24,7 +26,10 @@ const NavBar = () => {
           </Link>
         </li>
         <li>
-          <LogoutButton />
+          {currentUser ?
+           <LogoutButton /> : <Link to='/login' className='login-button'>Log In</Link>
+          }
+         
         </li>
       </ul>
     </nav>
