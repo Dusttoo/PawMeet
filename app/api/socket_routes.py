f5eded6c41880124fc48350698dc90a11564072d
Dusttoo <dusty.mumphrey@yahoo.com>
2022-11-02 16:34:51 -0500

    set up sockets on front and back end

---
+++ app/api/socket_routes.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, jsonify, request
+from flask_socketio import SocketIO, emit
+
+socketio = SocketIO(cors_allowed_origins="*")
+
+
+socket_routes = Blueprint('socket', __name__)
+
+@socket_routes.route("/http-call")
+def http_call():
+    """return JSON with string data as the value"""
+    data = {'data':'This text was fetched using an HTTP call to server on render'}
+    return jsonify(data)
+
+@socketio.on("connect")
+def connected():
+    """event listener when client connects to the server"""
+    print(request.sid)
+    print("client has connected")
+    emit("connect",{"data":f"id: {request.sid} is connected"})
+
+@socketio.on('data')
+def handle_message(data):
+    """event listener when client types a message"""
+    print("data from the front end: ",str(data))
+    emit("data",{'data':data,'id':request.sid},broadcast=True)
+
+@socketio.on("chat")
+def handle_chat(data):
+    emit("chat", data, broadcast=True)
+
+@socketio.on("disconnect")
+def disconnected():
+    """event listener when client disconnects to the server"""
+    print("user disconnected")
+    emit("disconnect",f"user {request.sid} disconnected",broadcast=True)
--- react-app/src/components/Landing/Landing.js
+++ react-app/src/components/Landing/Landing.js
@@ -22,6 +22,7 @@ import {
   getTenBreeds,
   getFivePosts,
 } from "../utils/helperFunctions";
+import Chat from "../Socket";
 
 const Landing = () => {
   const dispatch = useDispatch();
@@ -73,6 +74,7 @@ const Landing = () => {
                 </Link>
               )}
             </div>
+            <Chat />
             <div className="highlighted-breed-container">
               <ReactPlayer
                 width={"50%"}
